33cd2cfce4cd29136fa11b4c9fee2b8f7f42df68
helljav <miromancequimico_004@hotmail.com>
2019-05-27 11:47:12 -0500

    holi

--- src/Practica2/P2LenguajeDeProgramacion/P2LenguajesDeProgramacion/.dist-buildwrapper/src/Main.hs
+++ src/Practica2/P2LenguajeDeProgramacion/P2LenguajesDeProgramacion/.dist-buildwrapper/src/Main.hs
@@ -1,14 +1,21 @@
 --Cosntrutor para definir que los coeficientes
-data Fraccion = Coeficientes Int Int 
+data Fraccion = Coeficientes Int  Int 
                         deriving (Eq)
 
 instance Show Fraccion where
                 show(Coeficientes n d) = show(n) ++ "/"++ show (d)
 
-                                               
+
+mcd a 0 = a
+mcd a b = mcd b (a `mod` b)       
 
 
-resta(Coeficientes n1 d1) (Coeficientes n2 d2) = Coeficientes ((n1*d2)-(n2*d1)) (d1*d2)
+mcm a b = div (a*b) (mcd a b)                                     
+
+--resta(Coeficientes n1 d1) (Coeficientes n2 d2) = Coeficientes ((n1*d2)-(n2*d1)) (d1*d2)
+
+
+resta (Coeficientes n1 d1) (Coeficientes n2 d2) = Coeficientes (((div (mcm d1 d2) d1)*n1)-((div (mcm d1 d2) d2)*n2)) (mcm d1 d2)
 
 fraccionEgipcia n d = fEgipcia(Coeficientes n d) 
 
@@ -16,7 +23,7 @@ fraccionEgipcia n d = fEgipcia(Coeficientes n d)
 fEgipcia (Coeficientes 1 d) = [Coeficientes 1 d]
 fEgipcia (Coeficientes n d) 
                             |(n>1) = 
-                            (Coeficientes n d):(Coeficientes 1 ((div d n)+1)):fEgipcia ( resta (Coeficientes n d) (Coeficientes 1 ((div d n)+1) )) 
+                            (Coeficientes 1 ((div d n)+1)):fEgipcia ( resta (Coeficientes n d) (Coeficientes 1 ((div d n)+1) )) 
                                
                                  
 
